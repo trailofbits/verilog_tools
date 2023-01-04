64bd0ad4817830bb598deb4c42042b30fb75cd1f
jleightcap <jack@leightcap.com>
2023-01-04 13:49:46 -0500

    TASK(jl): init.
    Signed-off-by: jleightcap <jack@leightcap.com>

---
+++ default.nix
@@ -0,0 +1,25 @@
+let
+  pkgs = import (builtins.fetchTarball {
+    name = "nixpkgs-unstable-2022-07-19";
+    url = "https://github.com/nixos/nixpkgs/archive/2df37941652c28e0858b9a9520ce5763c43c2ec1.tar.gz";
+    sha256 = "sha256:12d5w1bvhjlxrvdhsc44gq1lv5s3z1lv18s39q1702hwmp2bz071";
+  }) {};
+
+  verilog_tools = with pkgs.python311.pkgs; buildPythonPackage rec {
+    pname = "verilog_tools";
+    version = "0.0.1";
+    src = ./.;
+    format = "setuptools";
+    propagatedBuildInputs = [ psutil ];
+  };
+  sv-python = pkgs.python311.withPackages (ps: with ps; [verilog_tools]);
+
+in
+with pkgs; stdenv.mkDerivation {
+  name = "sv-tools";
+  src = ./.;
+
+  nativeBuildInputs = [
+    sv-python
+  ];
+}
