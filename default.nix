9cb59aed5feba9c901b19ebd4dd9b8b0672b1a7e
Jack Leightcap <jack.leightcap@trailofbits.com>
2023-02-27 17:50:07 -0500

    TEST(jl): development dependencies
    Signed-off-by: Jack Leightcap <jack.leightcap@trailofbits.com>

--- default.nix
+++ default.nix
@@ -2,4 +2,16 @@
 , pkgs ? import sources.nixpkgs { }
 }:
 
-pkgs.callPackage ./derivation.nix { }
+let
+  verilog_tools = pkgs.callPackage ./derivation.nix { };
+in
+with pkgs; stdenv.mkDerivation {
+  name = "verilog_tools";
+
+  nativeBuildInputs = [
+    black
+    isort
+    mypy
+    ruff
+  ];
+}
